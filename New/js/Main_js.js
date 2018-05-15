e353d0ddf5086d5662680c25a716a64764aaa48c
1POTATO1 <38481771+1POTATO1@users.noreply.github.com>
2018-05-15 20:34:45 +0900

    Add files via upload

---
+++ New/js/Main_js.js
@@ -0,0 +1,18 @@
+function mouse_over(){
+  list1.style.color="red";
+}
+function mouse_out(){
+  list1.style.color="white";
+}
+function mouse_over1(){
+  list2.style.color="red";
+}
+function mouse_out1(){
+  list2.style.color="white";
+}
+function mouse_over2(){
+  list3.style.color="red";
+}
+function mouse_out2(){
+  list3.style.color="white";
+}
